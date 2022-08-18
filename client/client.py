516e50175a9859c2684b4e2365021a7bd978953a
NerosJourney <sandman4264@gmail.com>
2022-08-17 22:15:25 -0400

    Created basic tkinter client for communication with HTTP server

--- client/client.py
+++ client/client.py
@@ -1,9 +1,21 @@
 from tkinter import *
 from tkinter import ttk
+import requests
+
+resp = ''
+
+def reqFromServer():
+    global resp
+    r = requests.get('http://localhost:5000/dq/employees')
+    print(r.text)
+    resp = r
+
 
 root = Tk()
 frm = ttk.Frame(root, padding=25)
 frm.grid()
 ttk.Label(frm, text="Hello World!    ").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Button(frm, text="Request", command=reqFromServer).grid(column=0, row=1)
+ttk.Label(frm, text=resp).grid(column=1,row=1)
 root.mainloop()
