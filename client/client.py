bc894b26f430f33f2e43df3c2004d7a03df9c544
NerosJourney <sandman4264@gmail.com>
2022-08-18 22:00:52 -0400

    Fixed display of text in basic tkinter interface

--- client/client.py
+++ client/client.py
@@ -8,7 +8,8 @@ def reqFromServer():
     global resp
     r = requests.get('http://localhost:5000/dq/employees')
     print(r.text)
-    resp = r
+    resp = r.text
+    update_emps()
 
 
 root = Tk()
@@ -17,5 +18,11 @@ frm.grid()
 ttk.Label(frm, text="Hello World!    ").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 ttk.Button(frm, text="Request", command=reqFromServer).grid(column=0, row=1)
-ttk.Label(frm, text=resp).grid(column=1,row=1)
+emps = ttk.Label(frm, text=resp)
+emps.grid(column=1,row=1)
+
+def update_emps():
+    global emps
+    emps["text"] = resp
+
 root.mainloop()
--- server/server.py
+++ server/server.py
@@ -11,7 +11,7 @@ def hello_world():
 
 @app.route("/<company>/employees")
 def employed(company):
-    with open(f'{company}.emp', 'a+') as file:
+    with open(f'../db/{company}.emp', 'a+') as file:
         file.seek(0, 0)
         lines = file.readlines()
         lines = [line.rstrip() for line in lines]
@@ -19,17 +19,17 @@ def employed(company):
 
 @app.route("/<company>/employees/<name>")
 def addEmployee(company, name):
-    with open(f'{company}.emp', 'a+') as file:
+    with open(f'../db/{company}.emp', 'a+') as file:
         file.write(f'{name}\n')
     return f'Adding {name} as an employee'
 
 @app.route("/<company>/employees/fire/<name>")
 def fireEmployee(company, name):
-    with open(f'{company}.emp', 'a+') as file:
+    with open(f'../db/{company}.emp', 'a+') as file:
         file.seek(0, 0)
         lines = file.readlines()
         lines = [line.rstrip() for line in lines]
     lines.remove(f'{name}')
-    with open(f'{company}.emp', 'w') as file:
+    with open(f'../db/{company}.emp', 'w') as file:
         file.writelines(f'{emp}\n' for emp in lines)
     return f'You just fired {name} from {company}'
