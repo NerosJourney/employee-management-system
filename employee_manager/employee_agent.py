7c5c0a26c10de19b9958b23a62d4eef38abbea93
Bailey_Wimer <nerosjourney@gmail.com>
2022-08-22 20:05:22 -0400

    changed to use unittest python module

--- employee_manager/employee_agent.py
+++ employee_manager/employee_agent.py
@@ -16,6 +16,12 @@ class Employee:
     def set_skill(self, skill, num):
         self.skill_ratings[skill] = min(self.skill_bounds[1], max(self.skill_bounds[0], num))
 
+    def get_skill(self, skill) -> int:
+        if skill in self.skill_ratings:
+            return self.skill_ratings[skill]
+        else:
+            self.set_skill(skill, 0)
+            return 0
 
     def rate(self):
         global Skills
--- tests/employee_tests.py
+++ tests/employee_tests.py
@@ -1,12 +1,29 @@
-import sys
+import unittest, sys
 
-sys.path.insert(0, '../employee_manager')
+sys.path.insert(0, '/home/nero/code/personal/employee-management-system/employee_manager')
 
 from employee_agent import Employee
 from company_agent import Company
 
+class TestEmployeeGeneration(unittest.TestCase):
+    def test_employee_skill_assignments(self):
+        comp = Company("Test", 1234)
+        emp = Employee("John", comp)
+        emp.set_skill("Flippin' Burgers", 10)
+        emp.set_skill("Fry", 14)
+        emp.set_skill("Tacos", -10)
+        emp.set_skill("DQ", 0)
+        emp.set_skill("Shift Leader", 5)
+        emp.set_skill("Popcorn", -1000000000)
+        self.assertEqual(emp.get_skill("Flippin' Burgers"), 10)
+        self.assertEqual(emp.get_skill("Fry"), 10)
+        self.assertEqual(emp.get_skill("Tacos"), 0)
+        self.assertEqual(emp.get_skill("DQ"), 0)
+        self.assertEqual(emp.get_skill("Shift Leader"), 5)
+        self.assertEqual(emp.get_skill("Popcorn"), 0)
+        self.assertEqual(emp.name, "John")
+        self.assertEqual(emp.id, 0)
+        self.assertEqual(emp.company, comp)
+
 if __name__ == '__main__':
-    comp = Company('Test', 1234)
-    tom = Employee('tom', comp)
-    tom.rate()
-    tom.print_employee()
+    unittest.main()
